435deb9c586c6c144aec82dae62ac130920c2c98
Bruno Mendes <wolfteam1910@gmail.com>
2023-03-03 21:44:36 -0300

    config inicial do projeto + classes

---
+++ src/main/java/io/github/brunoyillli/apireactiva/ApiReactivaApplication.java
@@ -0,0 +1,13 @@
+package io.github.brunoyillli.apireactiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiReactivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiReactivaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/brunoyillli/apireactiva/ApiReactivaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.brunoyillli.apireactiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiReactivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
